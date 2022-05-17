c49ee1ff83974b80444a9a6de6cc55325433d2ab
OlivierWoerlee <105714310+OlivierWoerlee@users.noreply.github.com>
2022-05-17 13:28:48 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,6 +5,12 @@ var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
+  myOtherBox.object3D.rotation.set(
+    THREE.Math.degToRad(15),
+    THREE.Math.degToRad(30),
+    THREE.Math.degToRad(90)
+  );
+  
   myOtherBox.object3D.rotation.x += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
 }
